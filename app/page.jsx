2103b7eefdaf942155c0c155d1728bde980ed568
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2025-05-02 13:16:37 -0400

    Retirer les mentions Prototypes...

--- app/page.jsx
+++ app/page.jsx
@@ -4,9 +4,6 @@ export default async function Page() {
   
   return (
     <>
-      <h1>Cadavre exquis</h1>
-      <pre><code>Prototype 1.1</code></pre>
-      
       <FilmsGrille random></FilmsGrille>
     </>
   );
--- components/header.jsx
+++ components/header.jsx
@@ -8,7 +8,7 @@ const navItems = [
 
 export function Header() {
     return (
-        <nav className="flex flex-wrap items-center gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
+        <nav className="flex flex-wrap items-center gap-4 pt-6 pb-6">
             <div>
                 
             </div>
