19195221840f47bc13120cdc67448a44d7d3b0f5
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2025-05-09 15:51:11 -0400

    filmPage : correction du texte temporaire

--- components/filmPage.jsx
+++ components/filmPage.jsx
@@ -77,7 +77,7 @@ export function FilmPage( {path} ) {
       <Curated className='mb-6'>
         <hr className='mb-6' />
         <h2>À voir aussi</h2>
-        <p><i>[ Fiches d'autres films liés (curation) ]</i></p>
+        <p><i>[ Fiches d&apos;autres films liés (curation) ]</i></p>
       </Curated>
     </>
   );
