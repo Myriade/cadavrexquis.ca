bcfccd79c5000572f038637e61288bd70b2ff682
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2025-05-20 16:53:53 -0400

    FilmPage : Vimeo and more taxonomy data
    Improves the film page component by integrating Vimeo video display and fetching taxonomy data for dynamic content rendering.
    It also introduces a default film object to handle initial loading states.
    
    This change replaces the previous filmPage.jsx with a new version that includes:
    - Vimeo video integration, including handling loading states and extracting Vimeo IDs from URLs.
    - Taxonomy data fetching for film details like realization, languages, and thematics, using a utility function to find matching names.
    - A default film object to prevent errors during initial data loading.
    
    A utils.ts file extracts reusable function `findVocabularyTermNames`
    from the main component, to make it testable and more readable.
    
    The `useLoadTaxonomies` hook is updated to include the required taxonomies, and removes the need for params.

--- components/filmPage.jsx
+++ components/filmPage.jsx
@@ -3,8 +3,25 @@ import React, { useState, useRef, useEffect } from 'react'
 import styled from 'styled-components';
 import { drupal } from "/lib/drupal.ts"
 import { useLoadTaxonomies } from '../lib/fecthDrupalData'
+import { findVocabularyTermNames } from '../lib/utils.ts'
 
 const Styled = styled.section`
+
+  .vimeo {
+    background-color: black;
+    transition: all 0.5s;
+    min-height: 500px;}
+      
+  .vimeo.loading {
+    background-color: #777;
+    display: grid;
+    align-content: center;
+    justify-content: center;
+    color: white;}
+  
+  .vimeo iframe {
+    min-height: 500px;
+    width: 100%;}
 
   .type, .infos {
     color: var(--color-rouge);}
@@ -24,16 +41,81 @@ const Styled = styled.section`
 
 const Curated = styled.section``
 
-// const defautlFilm = {}
+const defautlFilm = {
+  "field_url_interne": [{uri: null}],
+  "field_annees_de_sortie": '... chargement',
+  "title": '\u00A0',
+  "field_descriptions_cadavrexquis": [{processed: ''}],
+  "type": null,
+  "id": null,
+  "drupal_internal__nid": null,
+  "path": {},
+  "created": null,
+  "changed": null,
+  "field_annees_de_production": null,
+  "field_commentaires": null,
+  "field_cote": null,
+  "field_credits_notes": null,
+  "field_dates_de_production_estime": false,
+  "field_descriptions_autres": [],
+  "field_duree": null,
+  "field_edge_code": null,
+  "field_emplacements": [],
+  "field_liens_avec_d_autres_films": [],
+  "field_lieux": [],
+  "field_longueur_metrage": null,
+  "field_longueur_nombre_de_bobines": null,
+  "field_longueur_pietage": null,
+  "field_notes": null,
+  "field_numero_identification": null,
+  "field_resume_de_l_institution_de": null,
+  "field_site_collection": null,
+  "field_site_visible": null,
+  "field_statut_legal": null,
+  "field_titres_alternatifs": [],
+  "field_titre_attribue": false,
+  "field_titre_de_serie": [],
+  "field_traitement": null,
+  "field_autres": [],
+  "field_commanditaires": [],
+  "field_consultants": [],
+  "field_direction_de_la_photograph": [],
+  "field_distribution": [],
+  "field_effets_speciaux_et_animati": [],
+  "field_emulsion": null,
+  "field_fabricant": null,
+  "field_format": null,
+  "field_format_de_production": [],
+  "field_institution_detentrice": null,
+  "field_jeu": [],
+  "field_langue": [],
+  "field_langues": [],
+  "field_montage": [],
+  "field_musique": [],
+  "field_narration": [],
+  "field_participation": [],
+  "field_pays_origine": [],
+  "field_personnes": [],
+  "field_production": [],
+  "field_ratio": null,
+  "field_realisation": [],
+  "field_scenario": [],
+  "field_site_photogramme": {},
+  "field_site_thematique": [],
+  "field_son": null,
+  "field_son_sound": [],
+  "field_type_d_image": null,
+  "field_vedettes_matiere": []
+}
 
 let realisation = ''
 let langues = ''
+let thematique = ''
+let vimeoSource = ''
 
 export function FilmPage( {path} ) {
-  const [ film, setFilm ] = useState(null)
-  
-  const taxoArray = ['realisation', 'langue']
-  const { data: taxonomyData, loading, error } = useLoadTaxonomies(taxoArray, 'taxonomiesCache')
+  const [ film, setFilm ] = useState(defautlFilm)
+  const { data: taxonomyData, loading, error } = useLoadTaxonomies()
   
   // Handle loading state
   if (loading) {
@@ -70,51 +152,55 @@ export function FilmPage( {path} ) {
     }
   }
   
-  if (!film) {
+  if (!film.type) {
     fetchFilm();
   } 
   
-  if (film && taxonomyData) {
-    // console.log('film.field_langue', film.field_langue)
-    // console.log('taxoData', taxonomyData)
+  if (film.type && taxonomyData) {
+    // console.log('film.field x output', field_url_interne)
     
-    function findMatchingNames(fieldArray, dataArray) {
-        const matchingNames = [];
+    realisation = findVocabularyTermNames(film.field_realisation, taxonomyData.realisation)
+    langues = findVocabularyTermNames(film.field_langue, taxonomyData.langue)
+    thematique = findVocabularyTermNames(film.field_site_thematique, taxonomyData.site_categorie)
+    
+    if (film.field_url_interne.length && film.field_url_interne[0] !== null) {
+      function getVimeoId(url) {
+        // Check if input is valid
+        if (!url || typeof url !== 'string') {
+          throw new Error('Invalid input: URL must be a non-empty string');
+        }
+    
+        // Match numbers after .com/
+        const match = url.match(/\.com\/(\d+)/);
         
-        // Loop through each item in fieldArray
-        fieldArray.forEach((fieldItem, fieldIndex) => {
-            // Find corresponding item in dataArray
-            const dataMatch = dataArray.find(dataItem => 
-                fieldItem.resourceIdObjMeta.drupal_internal__target_id === dataItem.id
-            );
-            
-            // If a match is found, add its name to the array
-            if (dataMatch && dataMatch.name) {
-                matchingNames.push(dataMatch.name);
-            }
-        });
+        if (!match) {
+          throw new Error('Invalid Vimeo URL format');
+        }
         
-        // Join all names with commas and return
-        return matchingNames.join(', ');
+        return match[1];
+      }    
+      const vimeoId = getVimeoId(film.field_url_interne[0].uri)
+      vimeoSource = `https://player.vimeo.com/video/${vimeoId}?badge=0&amp;byline=false&amp;title=false&amp;autopause=0&amp;player_id=0&amp;app_id=58479`
     }
-    
-    realisation = findMatchingNames(film.field_realisation, taxonomyData.realisation)
-    langues = findMatchingNames(film.field_langue, taxonomyData.langue)
-    // console.log(realisation);
   }
   
   return (
     <>
       <Styled>
-        <img src='/images/video-temp.png' alt='' className='video mb-8'/>
-        <p className='type text-xl mb-6'>Collection</p>
-        <h1 className='mb-6'>{film ? film.title : '...'}</h1>
+        { vimeoSource ? (
+          <div className='vimeo mb-6'>
+            <iframe src={vimeoSource} frameBorder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" title=""></iframe>
+          </div>
+        ): ( <div className='vimeo mb-6 loading'><span className='text-6xl'>...</span></div>)}
+        <script src="https://player.vimeo.com/api/player.js"></script>
+        <p className='type text-xl mb-6'>{film.field_site_collection}</p>
+        <h1 className='mb-6'>{film.title}</h1>
         <p className='infos text-xl font-sans mb-6'>
-          {film ? film.field_annees_de_sortie : 'chargement'} / 
-          <i> [un autre champ]</i>
+          {film.field_annees_de_sortie}
+          {taxonomyData && thematique ? (<> / <i>{thematique}</i></>) : ''}
         </p>
         <div 
-          dangerouslySetInnerHTML={film ? { __html: film.field_resume_de_l_institution_de.processed } : {__html: ''}}
+          dangerouslySetInnerHTML={ film.field_descriptions_cadavrexquis.length ? { __html: film.field_descriptions_cadavrexquis[0].processed } : {__html: '! Le champ « Descriptions et résumés de l’équipe de Cadavre exquis » est vide'}}
           className='texte text-lg font-serif mb-6'
         ></div>
         <dl className='mb-6'>
--- lib/utils.ts
+++ lib/utils.ts
@@ -10,3 +10,23 @@ export function formatDate(input: string): string {
 export function absoluteUrl(input: string) {
   return `${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}${input}`
 }
+
+export function findVocabularyTermNames(fieldArray, dataArray) {
+    const matchingNames = [];
+    
+    // Loop through each item in fieldArray
+    fieldArray.forEach((fieldItem, fieldIndex) => {
+        // Find corresponding item in dataArray
+        const dataMatch = dataArray.find(dataItem => 
+            fieldItem.resourceIdObjMeta.drupal_internal__target_id === dataItem.id
+        );
+        
+        // If a match is found, add its name to the array
+        if (dataMatch && dataMatch.name) {
+            matchingNames.push(dataMatch.name);
+        }
+    });
+    
+    // Join all names with commas and return
+    return matchingNames.join(', ');
+}
