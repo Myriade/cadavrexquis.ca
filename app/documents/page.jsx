0e04b7ee04ba3a820823c6903c6dd03bc381bc9e
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2025-06-09 23:05:06 -0400

    Adds placeholder pages
    Adds placeholder pages for "bibliographie", "collection",
    "documents", and "remontage" routes. These pages currently
    display a "coming soon" message.
    
    Also, adds margin to the 404 page content.

---
+++ app/documents/page.jsx
@@ -0,0 +1,11 @@
+export default async function Page() {
+  return (
+    <div>
+      <h1 className='mb-0'>Les documents</h1>
+      <div className="text-center grid absolute inset-0 content-center justify-center mb-6 mt-6">
+        <p>[À venir]</p>
+      </div>
+    </div>
+  );
+}
+
--- app/not-found.js
+++ app/not-found.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function NotFound() {
 	return (
-		<main className="text-center grid absolute inset-0 content-center justify-center">
+		<main className="text-center grid absolute inset-0 content-center justify-center mb-6 mt-6">
 			<h1 className="text-base uppercase mb-4">erreur 404</h1>
 			<p className="text-3xl text-muted-foreground mb-6">
 				Page non trouvée
