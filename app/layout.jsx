866111aa6f0fcab0f559bf308d7069e1bcd4e507
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2025-10-10 15:33:40 -0400

    Remets la protection mot de passe, parce que buggy sans

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,15 +20,17 @@ export default function RootLayout({ children }) {
         </head>
         
         <body className="antialiased">
-          <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
-            <div className="flex flex-col w-full max-w-7xl mx-auto grow">
-              <Header />
-              <div className="grow relative">
-                {children}
+          <TempProtectLayout>
+            <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
+              <div className="flex flex-col w-full max-w-7xl mx-auto grow">
+                <Header />
+                <div className="grow relative">
+                  {children}
+                </div>
+                <Footer />
               </div>
-              <Footer />
             </div>
-          </div>
+          </TempProtectLayout>
         </body>
       </html>
     </HeadTitleProvider>
