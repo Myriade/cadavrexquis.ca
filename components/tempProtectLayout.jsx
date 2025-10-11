deb08c4000e079bc24954283139bf59a60d60278
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2025-10-11 14:59:47 -0400

    Retire la protection mot de passe temporaire

--- components/tempProtectLayout.jsx
+++ components/tempProtectLayout.jsx
@@ -6,34 +6,40 @@ export function TempProtectLayout({children}) {
 
 	useEffect(() => {
 		// Vérifier si l'utilisateur est déjà authentifié
-		let localyAuthenticated = null;
+		let localyAuthenticated = true;
 		
-		if (localStorage.getItem('isAuthenticated')) {
-			localyAuthenticated = localStorage.getItem('isAuthenticated');
-		}
-		
-		const answer = process.env.NEXT_PUBLIC_TEMP_DEV_PROTECTION;
+		// if (localStorage.getItem('isAuthenticated')) {
+		// 	localyAuthenticated = localStorage.getItem('isAuthenticated');
+		// }
+		// 
+		// const answer = process.env.NEXT_PUBLIC_TEMP_DEV_PROTECTION;
 		
 		if (localyAuthenticated) {
 			setIsAuthenticated(true);
-		} else {
-			const verif = prompt('Authentification requise :');
-			if (verif === answer) {
-				localStorage.setItem('isAuthenticated', 'true');
-				setIsAuthenticated(true);
-			} else {
-				setIsAuthenticated(false);
-			}
-		}
+		} 
+		
+		// else {
+		// 	const verif = prompt('Authentification requise :');
+		// 	if (verif === answer) {
+		// 		localStorage.setItem('isAuthenticated', 'true');
+		// 		setIsAuthenticated(true);
+		// 	} else {
+		// 		setIsAuthenticated(false);
+		// 	}
+		// }
 	}, []);
 	
-	if (isAuthenticated === null) {
-		return ''
-	} else if (isAuthenticated) {
+	// if (isAuthenticated === null) {
+	// 	return ''
+	// } 
+	
+	if (isAuthenticated) {
 		return <>{children}</>
-	} else if (isAuthenticated === false) {
-		return <p>Authentification requise. Recharger la page.</p>
-	}
+	} 
+	
+	// if (isAuthenticated === false) {
+	// 	return <p>Authentification requise. Recharger la page.</p>
+	// }
 };
 
 export default TempProtectLayout;
