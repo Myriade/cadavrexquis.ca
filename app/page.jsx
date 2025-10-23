1cdd7ab608a5ba5f23bad2ed9f48c37702e94ffa
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2025-10-23 10:51:01 -0400

    SEO : Configures metadata and OG tags on Accueil
    Sets up metadata for improved SEO and social sharing,
    including title, description, and open graph properties.
    
    Also adds a landing page displaying the content loader and mission statement.

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,34 @@ import ContentLoader from '../components/contentLoader';
 import Mission from '../components/mission';
 
 export const metadata = {
-  title: 'Cadavre exquis',
-  description: 'Cadavre exquis, “ouvroir de cinéma potentiel” (OUCIPO), est un laboratoire de valorisation patrimoniale et de création expérimentale, dédié au cinéma scientifique et éducatif. Il a été initié par la revue Hors champ en 2021',
+  title: 'Cadavre exquis, “ouvroir de cinéma potentiel” (OUCIPO)',
+  description: 'Cadavre exquis est un laboratoire de valorisation patrimoniale et de création expérimentale, dédié au cinéma scientifique et éducatif.',
+  openGraph: {
+    title: 'Cadavre exquis, “ouvroir de cinéma potentiel” (OUCIPO)',
+    description: 'Cadavre exquis est un laboratoire de valorisation patrimoniale et de création expérimentale, dédié au cinéma scientifique et éducatif.',
+    url: 'https://cadavrexquis.ca/',
+    siteName: 'Cadavre exquis',
+    images: [
+      {
+        url: 'https://cadavrexquis.ca/cadavrexquis.ca-og.png',
+        width: 1200,
+        height: 620,
+        alt: 'Cadavre Exquis, Sciences naturelles, Santé et sciences de la vie humaine, Mathématiques, Technologie',
+      },
+    ],
+    videos: [
+      {
+        url: 'https://vimeo.com/1102788773/86739cff75',
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: 'fr_CA',
+    type: 'website',
+  },
+  alternates: {
+    canonical: 'https://cadavrexquis.ca/'
+  }
 }
 
 export default function Page() {
