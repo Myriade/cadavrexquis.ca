c51c33ae801b2d40a879435d4483523c66029bfa
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2025-12-10 15:27:37 -0500

    Matomo: réparer le tracking

--- app/layout.jsx
+++ app/layout.jsx
@@ -32,19 +32,19 @@ export default function RootLayout({ children }) {
         </head>
         
         <body className="antialiased">
+          <TempProtectLayout>
+            <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
+              <div className="flex flex-col w-full max-w-7xl mx-auto grow">
+                <Header />
+                <div className="grow relative">
+                  {children}
+                </div>
+                <Footer />
+              </div>
+            </div>
+          </TempProtectLayout>
           <Suspense fallback={null}>
             <MatomoAnalytics />
-            <TempProtectLayout>
-              <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
-                <div className="flex flex-col w-full max-w-7xl mx-auto grow">
-                  <Header />
-                  <div className="grow relative">
-                    {children}
-                  </div>
-                  <Footer />
-                </div>
-              </div>
-            </TempProtectLayout>
           </Suspense>
         </body>
       </html>
